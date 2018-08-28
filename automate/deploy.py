509bcd28e3c4d053405b8e2cb7f3b0b97f974acc
Benjamin Copeland <ben.copeland@linaro.org>
2018-08-28 23:18:27 +0100

    Improve python script for deployment

--- automate/deploy.py
+++ automate/deploy.py
@@ -1,11 +1,25 @@
-import os, time
-path_to_watch = "/mnt/data/completed/TV"
-before = dict ([(f, None) for f in os.listdir (path_to_watch)])
-while 1:
-  time.sleep (10)
-  after = dict ([(f, None) for f in os.listdir (path_to_watch)])
-  added = [f for f in after if not f in before]
-  removed = [f for f in before if not f in after]
-  if added: print "Added: ", ", ".join (added)
-  if removed: print "Removed: ", ", ".join (removed)
-  before = after
+import os
+import subprocess
+import shlex
+
+path_to_watch = '/mnt/data/completed/TV'
+get_iplayer = '/home/bhcopeland/.get_iplayer/pvr'
+
+tv_shows = [f for f in os.listdir (get_iplayer)]
+for show in tv_shows:
+    print(show)
+    p = subprocess.Popen("get_iplayer --pvr --overwrite " + show, shell=True, stdout=subprocess.PIPE)
+    (output, err) = p.communicate()
+    p_status = p.wait()
+    print output
+    for file in os.listdir(path_to_watch):
+        if file.endswith(".mp4"):
+            print(file)
+            f = subprocess.Popen("/opt/flexget/venv/bin/flexget --cron -c /opt/flexget/config_sorter.yml execute", shell=True, stdout=subprocess.PIPE)
+            (output, err) = f.communicate()
+            f_status = f.wait()
+            print output
+	    r = subprocess.Popen("flock -n /tmp/.rlock_lock rclone move /var/data/ gstorage:/Plex/ -vvv", shell=True, stdout=subprocess.PIPE)
+	    (output, err) = r.communicate()
+            r_status = r.wait()
+            print output
