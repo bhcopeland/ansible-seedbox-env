122b89f1a0ece0a41c5bcf8eaf3419047a938e2d
Benjamin Copeland <ben@copeland.me.uk>
2018-09-28 17:19:25 +0000

    make iplayer a deployable option update rclone

--- ansible_deploy.sh
+++ ansible_deploy.sh
@@ -6,7 +6,15 @@
    gather_facts: false
    become: yes
 ##   ignore_errors: yes
+   vars_prompt:
+     - name: "configure_iplayer"
+       prompt: "setup get_iplayer?"
+
    tasks:
+     - name: install iplayer
+       include: get_iplayer.yml
+       when: configure_iplayer.0 is defined
+
      - name: install deps
        include: deps.yml
        tags: deps
