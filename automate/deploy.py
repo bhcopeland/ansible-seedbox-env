4247d6e8da4f0eb5d808e1f85d8dd600eab33452
Benjamin Copeland <ben@copeland.me.uk>
2018-11-20 13:03:47 +0000

    iplayer path update update autmoate script update iplayer syncs

--- automate/deploy.py
+++ automate/deploy.py
@@ -2,8 +2,14 @@ import os
 import subprocess
 import shlex
 
-path_to_watch = '/mnt/data/completed/TV'
+def files(path):
+    for file in os.listdir(path):
+        if os.path.isfile(os.path.join(path, file)):
+            yield file
+
+path_downloaded = '/mnt/data/completed/TV'
 get_iplayer = '/home/bhcopeland/.get_iplayer/pvr'
+path_completed = '/var/data/TV'
 
 tv_shows = [f for f in os.listdir (get_iplayer)]
 for show in tv_shows:
@@ -12,13 +18,13 @@ for show in tv_shows:
     (output, err) = p.communicate()
     p_status = p.wait()
     print output
-    for file in os.listdir(path_to_watch):
-        if file.endswith(".mp4"):
-            print(file)
-            f = subprocess.Popen("/opt/flexget/venv/bin/flexget --cron -c /opt/flexget/config_sorter.yml execute", shell=True, stdout=subprocess.PIPE)
+    for files in os.listdir(path_downloaded):
+        if files.endswith(".mp4"):
+            f = subprocess.Popen("/opt/flexget/venv/bin/flexget -c /opt/flexget/config_sorter.yml execute", shell=True, stdout=subprocess.PIPE)
             (output, err) = f.communicate()
             f_status = f.wait()
             print("Flexget", output)
+
 	    r = subprocess.Popen("flock -n /tmp/.rlock_lock rclone move /var/data/ gstorage:/Plex/ -vvv", shell=True, stdout=subprocess.PIPE)
 	    (output, err) = r.communicate()
             r_status = r.wait()
