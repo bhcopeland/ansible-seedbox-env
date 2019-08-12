eee1215030eddd4981ed75f8abf1cdabbde505d9
Benjamin Copeland <ben@copeland.me.uk>
2019-08-12 14:42:09 +0000

    iplayer update

--- automate/deploy.py
+++ automate/deploy.py
@@ -12,6 +12,11 @@ get_iplayer = '/home/bhcopeland/.get_iplayer/pvr'
 path_completed = '/var/data/TV'
 
 tv_shows = [f for f in os.listdir (get_iplayer)]
+for files in os.listdir(path_downloaded):
+    f = subprocess.Popen("/opt/flexget/venv/bin/flexget -c /opt/flexget/config_sorter.yml execute", shell=True, stdout=subprocess.PIPE)
+    (output, err) = f.communicate()
+    f_status = f.wait()
+    print("Flexget", output)
 for show in tv_shows:
     print(show)
     p = subprocess.Popen("get_iplayer --pvr " + show, shell=True, stdout=subprocess.PIPE)
@@ -21,13 +26,10 @@ for show in tv_shows:
     for file_com in os.listdir(path_completed):
         sub = path_completed + '/' + file_com
         for file_coms in os.listdir(sub):
-            r = subprocess.Popen("flock -n /tmp/.rlock_lock rclone move /var/data/ gstorage:/Plex/ -vvv", shell=True, stdout=subprocess.PIPE)
-	    (output, err) = r.communicate()
-	    r_status = r.wait()
-            print("rclone", output)
-    for files in os.listdir(path_downloaded):
-        if files.endswith(".mp4"):
-            f = subprocess.Popen("/opt/flexget/venv/bin/flexget -c /opt/flexget/config_sorter.yml execute", shell=True, stdout=subprocess.PIPE)
-            (output, err) = f.communicate()
-            f_status = f.wait()
-            print("Flexget", output)
+       # if files.endswith(".mp4"):
+           r = subprocess.Popen("flock -n /tmp/.rlock_lock rclone move /var/data/ gstorage:/Plex/ -vvv", shell=True, stdout=subprocess.PIPE)
+	   (output, err) = r.communicate()
+	   r_status = r.wait()
+           print("rclone", output)
+
+
