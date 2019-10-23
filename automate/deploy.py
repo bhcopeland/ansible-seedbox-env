919df22ded2242d1bf8c9d26e498c904c8381d40
Benjamin Copeland <ben@copeland.me.uk>
2019-10-23 09:18:57 +0000

    iplayer update

--- automate/deploy.py
+++ automate/deploy.py
@@ -11,25 +11,39 @@ path_downloaded = '/mnt/data/completed/TV'
 get_iplayer = '/home/bhcopeland/.get_iplayer/pvr'
 path_completed = '/var/data/TV'
 
-tv_shows = [f for f in os.listdir (get_iplayer)]
-for files in os.listdir(path_downloaded):
+def upgrade_iplayer():
+    u = subprocess.Popen("sudo apt install get-iplayer -y", shell=True, stdout=subprocess.PIPE)
+    (output, err) = u.communicate()
+    u_status = u.wait()
+
+def run_flexget():
     f = subprocess.Popen("/opt/flexget/venv/bin/flexget -c /opt/flexget/config_sorter.yml execute", shell=True, stdout=subprocess.PIPE)
     (output, err) = f.communicate()
     f_status = f.wait()
-    print("Flexget", output)
-for show in tv_shows:
-    print(show)
+    return output
+
+def run_rclone():
+    r = subprocess.Popen("flock -n /tmp/.rlock_lock rclone move /var/data/ gstorage:/Plex/ -vvv", shell=True, stdout=subprocess.PIPE)
+    (output, err) = r.communicate()
+    r_status = r.wait()
+
+def run_iplayer():
     p = subprocess.Popen("get_iplayer --pvr " + show, shell=True, stdout=subprocess.PIPE)
     (output, err) = p.communicate()
     p_status = p.wait()
-    print output
-    for file_com in os.listdir(path_completed):
-        sub = path_completed + '/' + file_com
-        for file_coms in os.listdir(sub):
+         
+tv_shows = [f for f in os.listdir (get_iplayer)]
+for files in os.listdir(path_downloaded):
+   run_flexget()
+#   print("Flexget", output)
+for show in tv_shows:
+   print(show)
+   run_iplayer()
+   for file_com in os.listdir(path_completed):
+       sub = path_completed + '/' + file_com
+       for file_coms in os.listdir(sub):
+           run_rclone()
        # if files.endswith(".mp4"):
-           r = subprocess.Popen("flock -n /tmp/.rlock_lock rclone move /var/data/ gstorage:/Plex/ -vvv", shell=True, stdout=subprocess.PIPE)
-	   (output, err) = r.communicate()
-	   r_status = r.wait()
-           print("rclone", output)
+    #      print("rclone", output)
 
 
