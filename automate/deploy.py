b2680636cf6531a9d3a9799d58baa5640d0809e5
Benjamin Copeland <ben.copeland@linaro.org>
2021-06-10 09:55:28 +0100

    Update... massively out of date
    Signed-off-by: Benjamin Copeland <ben.copeland@linaro.org>

--- automate/deploy.py
+++ automate/deploy.py
@@ -8,7 +8,7 @@ def files(path):
             yield file
 
 path_downloaded = '/mnt/data/completed/TV'
-get_iplayer = '/home/bhcopeland/.get_iplayer/pvr'
+get_iplayer = '/home/ben/.get_iplayer/pvr'
 path_completed = '/var/data/TV'
 
 def upgrade_iplayer():
@@ -17,21 +17,21 @@ def upgrade_iplayer():
     u_status = u.wait()
 
 def run_flexget():
-    f = subprocess.Popen("/opt/flexget/venv/bin/flexget -c /opt/flexget/config_sorter.yml execute", shell=True, stdout=subprocess.PIPE)
+    f = subprocess.Popen("venv/bin/flexget -c files/flexget/config_sorter.yml execute", shell=True, stdout=subprocess.PIPE)
     (output, err) = f.communicate()
     f_status = f.wait()
     return output
 
 def run_rclone():
-    r = subprocess.Popen("/usr/bin/flock -n /tmp/.rlock_lock /usr/local/bin/rclone move /var/data/ gstorage:/Plex/ -vvv", shell=True, stdout=subprocess.PIPE)
+    r = subprocess.Popen("/usr/bin/flock -n /tmp/.rlock_lock rclone move /var/data/ gstorage:/Plex/ -vvv", shell=True, stdout=subprocess.PIPE)
     (output, err) = r.communicate()
     r_status = r.wait()
 
 def run_iplayer():
-    p = subprocess.Popen("/usr/bin/get-iplayer --pvr " + show, shell=True, stdout=subprocess.PIPE)
+    p = subprocess.Popen("/usr/bin/get_iplayer --pvr " + show, shell=True, stdout=subprocess.PIPE)
     (output, err) = p.communicate()
     p_status = p.wait()
-         
+
 tv_shows = [f for f in os.listdir (get_iplayer)]
 for files in os.listdir(path_downloaded):
    run_flexget()
@@ -39,10 +39,10 @@ for files in os.listdir(path_downloaded):
 for show in tv_shows:
    print(show)
    run_iplayer()
-   for file_com in os.listdir(path_completed):
-       sub = path_completed + '/' + file_com
-       for file_coms in os.listdir(sub):
-           run_rclone()
+#   for file_com in os.listdir(path_completed):
+#       sub = path_completed + '/' + file_com
+#       for file_coms in os.listdir(sub):
+#           run_rclone()
        # if files.endswith(".mp4"):
     #      print("rclone", output)
 
