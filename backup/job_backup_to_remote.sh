cc904104edf0068d4791c664b711c1cc3cbf5fb4
Benjamin Copeland <ben@copeland.me.uk>
2021-10-27 13:20:12 +0200

    Add backup

---
+++ backup/job_backup_to_remote.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+export rclone_jobber="rclone_jobber"
+
+rclone_jobber=$rclone_jobber
+
+source="/opt"
+dest="gstorage:/backup/seedbox"
+move_old_files_to="dated_files"
+options="--filter-from=filter_rules --checksum -P --transfers=20 --checkers 20"
+monitoring_URL=""
+
+$rclone_jobber/rclone_jobber.sh "$source" "$dest" "$move_old_files_to" "$options" "$(basename $0)" "$monitoring_URL"
