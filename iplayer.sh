327efe237b750a0d54b75e7a9d9720271f16d071
root <root@cctv>
2022-03-03 10:44:44 +0000

    Add zoneminder, and remove flexget from deployment

--- iplayer.sh
+++ iplayer.sh
@@ -3,4 +3,4 @@
 #get_iplayer --pvr && pipenv run flexget -c files/flexget/config_sorter.yml execute && rclone move /var/data/ gstorag    e:/Plex/ -vvv
 
 
-docker run -v $(pwd)/files/get_iplayer:/data/config -v /mnt/data/completed/TV:/data/output barwell/get-iplayer --pvr && pipenv run flexget -c files/flexget/config_sorter.yml execute && rclone move /var/data/ gstorage:/Plex/ -vvv
+cd /home/ben/ansible-seedbox-env && docker run -v $(pwd)/files/get_iplayer:/data/config -v /mnt/data/completed/TV:/data/output barwell/get-iplayer --pvr && pipenv run flexget -c files/flexget/config_sorter.yml execute && rclone move /var/data/ gstorage:/Plex/ -vvv
