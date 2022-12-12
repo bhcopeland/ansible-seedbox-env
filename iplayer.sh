15ac127fa76688477a6d92d7c73cdb8158cfc35e
Benjamin Copeland <ben.copeland@linaro.org>
2022-12-12 11:55:57 +0000

    Add duckdns and update iplayer
    Signed-off-by: Benjamin Copeland <ben.copeland@linaro.org>

--- iplayer.sh
+++ iplayer.sh
@@ -2,5 +2,8 @@
 
 #get_iplayer --pvr && pipenv run flexget -c files/flexget/config_sorter.yml execute && rclone move /var/data/ gstorag    e:/Plex/ -vvv
 
-
-cd /home/ben/ansible-seedbox-env && docker run -v $(pwd)/files/get_iplayer:/data/config -v /mnt/data/completed/TV:/data/output barwell/get-iplayer --pvr && pipenv run flexget -c files/flexget/config_sorter.yml execute && rclone move /var/data/ gstorage:/Plex/ -vvv
+docker pull barwell/get-iplayer:latest
+cd /home/ben/GitHub/ansible-seedbox-env
+docker run -v $(pwd)/files/get_iplayer:/data/config -v /mnt/data/completed/TV:/data/output barwell/get-iplayer --pvr
+pipenv run flexget -c files/flexget/config_sorter.yml execute
+rclone move /var/data/ gstorage:/Plex/ -vvv
