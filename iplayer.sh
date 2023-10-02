7dc1bcd320bb4860e8dff4f3d457ab30f1b3ee6b
Benjamin Copeland <ben.copeland@linaro.org>
2023-10-02 09:47:47 +0100

    iplayer update
    Signed-off-by: Benjamin Copeland <ben.copeland@linaro.org>

--- iplayer.sh
+++ iplayer.sh
@@ -6,4 +6,5 @@ docker pull barwell/get-iplayer:latest
 cd /home/ben/GitHub/ansible-seedbox-env
 docker run -v $(pwd)/files/get_iplayer:/data/config -v /mnt/data/completed/TV:/data/output barwell/get-iplayer --pvr
 pipenv run flexget -c files/flexget/config_sorter.yml execute
-rclone move /var/data/ gstorage:/Plex/ -vvv
+#rclone move /var/data/ gstorage:/Plex/ -vvv
+rsync -avhe ssh /var/data/TV/ bhcopeland@192.168.0.242:/mnt/media/TV/
